b26a3a67ed173742290e443453664065f5e4aa67
Allen Wang <wangallen98@gmail.com>
2019-06-02 20:05:13 -0500

    part 3, templates, template inheritance, bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 2, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 3, 2019'
+    }
+]
 
 # handle when user goes to blog home page
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # keyname accessible within template
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
